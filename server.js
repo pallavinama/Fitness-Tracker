9078dabdf345ae43fa17f2bc60568ff06bebdc2d
pallavi <pallavi_nama@yahoo.com>
2020-04-23 16:54:33 -0400

    server js file and npm install setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express"); 
+const mongoose = require("mongoose");  
+const PORT = process.env.PORT || 3000; 
+const app = express(); 
+ 
+ 
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
+ 
+app.use(express.static("Develop/public")); 
+ 
+ 
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true }) 
+//    .then(() => console.log("MongoDB successfully connected")) 
+//   .catch(err => console.log(err)); 
+
+ // routes 
+// require("./routes/apiRoutes")(app); 
+// require("./routes/htmlRoutes")(app); 
+ 
+ 
+ app.listen(PORT, () => { 
+   console.log(`App running on port ${PORT}!`); 
+ }); 
